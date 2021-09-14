f1b0d313674e324dd38e0a72e8023df09236a31b
Nam Hee Gordon Kim <namhee.kim@aalto.fi>
2021-09-14 12:16:49 +0300

    Apply external force via velocity edit

--- js/system.js
+++ js/system.js
@@ -1,11 +1,11 @@
 import * as THREE from 'https://threejs.org/build/three.module.js';
 
-const basicMaterial = new THREE.MeshPhongMaterial({color: 0x665544});
-const targetMaterial = new THREE.MeshPhongMaterial({color: 0xff2222});
+const basicMaterial = new THREE.MeshPhongMaterial({color: 0x665544, wireframe: true});
+const targetMaterial = new THREE.MeshPhongMaterial({color: 0xff2222, wireframe: true});
 const gridTexture = new THREE.TextureLoader().load("/brax/js/assets/grid.png");
 const gridMaterial = new THREE.MeshLambertMaterial({ map: gridTexture, color: 0xe0e0e0 })
 
-function createCapsule(capsule) {
+function createCapsule(capsule, name) {
   console.log("createCapsule invoked")
   console.log("gridMaterial")
 
@@ -13,20 +13,21 @@ function createCapsule(capsule) {
   const cylinder_geom = new THREE.CylinderGeometry(
       capsule.radius, capsule.radius, capsule.length - 2 * capsule.radius);
 
-  // const sphere1 = new THREE.Mesh(sphere_geom, basicMaterial);
-  const sphere1 = new THREE.Mesh(sphere_geom, gridMaterial);
+  let mat = name == '$ Goal' ? targetMaterial : basicMaterial;
+  const sphere1 = new THREE.Mesh(sphere_geom, mat);
+  // const sphere1 = new THREE.Mesh(sphere_geom, gridMaterial);
   sphere1.baseMaterial = sphere1.material;
   sphere1.position.set(0, capsule.length / 2 - capsule.radius, 0);
   sphere1.castShadow = true;
 
-  // const sphere2 = new THREE.Mesh(sphere_geom, basicMaterial);
-  const sphere2 = new THREE.Mesh(sphere_geom, gridMaterial);
+  const sphere2 = new THREE.Mesh(sphere_geom, mat);
+  // const sphere2 = new THREE.Mesh(sphere_geom, gridMaterial);
   sphere2.baseMaterial = sphere2.material;
   sphere2.position.set(0, -capsule.length / 2 + capsule.radius, 0);
   sphere2.castShadow = true;
 
-  // const cylinder = new THREE.Mesh(cylinder_geom, basicMaterial);
-  const cylinder = new THREE.Mesh(cylinder_geom, gridMaterial);
+  const cylinder = new THREE.Mesh(cylinder_geom, mat);
+  // const cylinder = new THREE.Mesh(cylinder_geom, gridMaterial);
   cylinder.baseMaterial = cylinder.material;
   cylinder.castShadow = true;
 
@@ -47,24 +48,24 @@ function createBox(box) {
 function createPlane(plane) {
   const group = new THREE.Group();
   const mesh = new THREE.Mesh(
-      new THREE.PlaneGeometry(2000, 2000),
+      new THREE.PlaneGeometry(2.24, 1.12),
       new THREE.MeshPhongMaterial({color: 0x999999, depthWrite: false}));
   mesh.rotation.x = -Math.PI / 2;
   mesh.receiveShadow = true;
   mesh.baseMaterial = mesh.material;
   group.add(mesh);
 
-  const mesh2 = new THREE.GridHelper(2000, 2000, 0x000000, 0x000000);
+  const mesh2 = new THREE.GridHelper(20, 20, 0x000000, 0x000000);
   mesh2.material.opacity = 0.4;
   mesh2.material.transparent = true;
   mesh2.baseMaterial = mesh2.material;
-  group.add(mesh2);
+  // group.add(mesh2);
   return group;
 }
 
 function createSphere(sphere, name) {
   const geom = new THREE.SphereGeometry(sphere.radius, 16, 16);
-  let mat = name.toLowerCase() == 'target' ? targetMaterial : basicMaterial;
+  let mat = name.toLowerCase() == 'target' ? targetMaterial : gridMaterial;
   const mesh = new THREE.Mesh(geom, mat);
   mesh.castShadow = true;
   mesh.baseMaterial = mesh.material;
@@ -111,7 +112,7 @@ function createScene(system) {
       if ('box' in collider) {
         child = createBox(collider.box);
       } else if ('capsule' in collider) {
-        child = createCapsule(collider.capsule);
+        child = createCapsule(collider.capsule, body.name);
       } else if ('plane' in collider) {
         child = createPlane(collider.plane);
       } else if ('sphere' in collider) {
