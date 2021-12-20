e80e5bb39b745d0c821663e2fd73adcc700f4397
namheegordonkim <namhee.kim@aalto.fi>
2021-12-20 11:20:01 +0200

    Add visual files

--- js/viewer.js
+++ js/viewer.js
@@ -5,11 +5,13 @@
 
 import * as THREE from 'https://threejs.org/build/three.module.js';
 import {OrbitControls} from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
-import {GUI} from 'https://threejs.org/examples/jsm/libs/dat.gui.module.js';
+import {GUI} from 'https://threejs.org/examples/jsm/libs/lil-gui.module.min.js';
 
 import {Animator} from './animator.js';
 import {Selector} from './selector.js';
 import {createScene, createTrajectory} from './system.js';
+
+console.log("viewer.js")
 
 function downloadDataUri(name, uri) {
   let link = document.createElement('a');
@@ -49,7 +51,7 @@ class Viewer {
     this.trajectory = createTrajectory(system);
 
     /* set up renderer, camera, and add default scene elements */
-    this.renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+    this.renderer = new THREE.WebGLRenderer({antialias: true, alpha: true, preserveDrawingBuffer: true});
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.shadowMap.enabled = true;
     this.renderer.outputEncoding = THREE.sRGBEncoding;
@@ -57,8 +59,8 @@ class Viewer {
     this.domElement.appendChild(this.renderer.domElement);
 
     this.camera = new THREE.PerspectiveCamera(40, 1, 0.01, 100);
-    this.camera.position.set(5, 2, 8);
-    this.camera.follow = true;
+    this.camera.position.set(0, 5, 0);
+    this.camera.follow = false;
     this.camera.freezeAngle = false;
     this.camera.followDistance = 10;
 
@@ -84,9 +86,10 @@ class Viewer {
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     this.controls.enablePan = false;
     this.controls.enableDamping = true;
-    this.controls.addEventListener('start', () => {this.setDirty();});
-    this.controls.addEventListener('change', () => {this.setDirty();});
+    this.controls.addEventListener('start', () => {this.setDirty()});
+    this.controls.addEventListener('change', () => {this.setDirty()});
     this.controlTargetPos = this.controls.target.clone();
+    this.controls.enabled = false;
 
     /* set up gui */
     this.gui = new GUI({autoPlace: false});
@@ -160,9 +163,6 @@ class Viewer {
     window.addEventListener('resize', (evt) => this.setSize(), false);
     requestAnimationFrame(() => this.setSize());
 
-    const resizeObserver = new ResizeObserver(() => this.resizeCanvasToDisplaySize());
-    resizeObserver.observe(this.domElement, {box: 'content-box'});
-
     /* start animation */
     this.animate();
   }
@@ -187,13 +187,6 @@ class Viewer {
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(w, h);
     this.setDirty();
-  }
-
-  resizeCanvasToDisplaySize() {
-    //look up canvas size
-    const width = this.domElement.clientWidth;
-    const height = this.domElement.clientHeight;
-    this.setSize(width, height);
   }
 
   render() {
